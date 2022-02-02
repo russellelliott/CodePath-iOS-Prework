6313bbf856c6ecff7a54924bfa63b9dabbe3ee2e
Russell Elliott <russell@quadsquad.net>
2022-02-01 17:13:04 -0800

    'Change Text String' button is able to change the text field.

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Input field to enter Text String
+    @IBOutlet weak var TextInput: UITextField!
+    
+    //Default value of the text string
+    var text = "Enter text in the field above"
+    
+    //Text String displayed
+    @IBOutlet weak var TextStringDisplay: UILabel!
+    
+    //Code for "Change Text String" Button
+    @IBAction func ChangeString(_ sender: Any) {
+        //Change text of the Text String
+        TextStringDisplay.text = "new text! :)"
+    }
+    
+    //Code for "Change Background Color" Button
+    @IBAction func ChangeBackground(_ sender: Any) {
+    }
+    
+    //Code for "Change Text Color" Button
+    @IBAction func ChangeColor(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
